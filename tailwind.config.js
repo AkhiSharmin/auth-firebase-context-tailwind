40907b2da8ad65b308adb54d7eaee3de0aba369c
AkhiSharmin <akhisharmin100@gmail.com>
2023-04-19 12:12:20 +0600

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,35 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+
+          "primary": "#049624",
+
+          "secondary": "#a4ef8b",
+
+          "accent": "#3abcba",
+
+          "neutral": "#392442",
+
+          "base-100": "#E5E5F1",
+
+          "info": "#8AC9EA",
+
+          "success": "#79ECAD",
+
+          "warning": "#A66908",
+
+          "error": "#F33551",
+        },
+      },
+    ],
+  },
+  plugins: [require("daisyui")],
+}
+
